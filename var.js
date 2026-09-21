40fccff3889f065107cb26b261e4d5ea488c6ff8
Bobbyprogrammer <babardev167@gmail.com>
2026-09-21 22:42:52 +0500

    variable ,datatype,typeof

---
+++ var.js
@@ -0,0 +1,51 @@
+// let name = "Deepak";
+
+// name = "Babar";
+// let age = 24;
+
+// age = 25;
+// console.log(name);
+// console.log(age);
+
+// var name = "Deepak";
+// var age = 25;
+
+// console.log(name);
+// console.log(age);
+
+// const country = "India";
+
+// country = "Pakistan";
+
+// Data Types
+
+// const name = "Depak"; // string
+
+// const age = 22; // number
+
+// const isStudent = false; // boolean
+
+// const address = null; //  null
+
+// let phone; // undefined
+
+// const skills = ["HTML", "CSS", "JS"]; // array
+
+// const user = {
+//   name: "Babar",
+//   age: 22,
+// }; // object
+
+// typeof  Operator
+
+let username = "Babar";
+
+let age = 20;
+
+const isStudent = false;
+
+console.log(typeof username);
+
+console.log(typeof age);
+
+console.log(typeof isStudent);
