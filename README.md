1aaec820d1445f08b35bceaf742cd60b569ac320
Bobbyprogrammer <babardev167@gmail.com>
2026-09-22 22:44:48 +0500

    operators

---
+++ README.md
@@ -0,0 +1,12 @@
+1. Javascripts Basics
+
+2. Data Types
+
+3. typeof Operator
+
+4. Operators and Conditions
+   Arithmetic:
+   Comparison:
+   Conditions:
+   Logical:
+   ternary:
---
+++ operators.js
@@ -0,0 +1,65 @@
+// Arithmetic
+// const a = 20;
+// const b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Comparison
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 === 10);
+// console.log(10 !== 5);
+
+// == vs ===
+// console.log(5 == "5"); // true
+// console.log(5 === "5"); // false
+
+// Conditions
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// const marks = 50;
+
+// if (marks >= 80) {
+//   console.log("A");
+// } else if (marks >= 70) {
+//   console.log("B");
+// } else if (marks >= 60) {
+//   console.log("C");
+// } else {
+//   console.log("Fail");
+// }
+
+// Logical operators
+
+// const age = 22;
+// const hasAdharCard = false;
+
+// if (age >= 18 || hasAdharCard) {
+//   console.log("Allowed");
+// } else {
+//   console.log("Not Allowed");
+// }
+
+// ternary
+// const age = 15;
+
+// const status = age >= 18 ? "Adult" : "Minor";
+
+// console.log(status);
+
+// if (age >= 18) {
+//   ("Adult");
+// } else {
+//   console.log("Minor");
+// }
